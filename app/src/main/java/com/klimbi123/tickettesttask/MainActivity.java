7d03eb661be01f04fdc8e9c13b47b4059c5a6ac3
Ingmar Trump <datklimbi123@gmail.com>
2020-11-19 15:45:05 +0200

    feat: Protobuf added to Gradle
    Ticket message generated

--- app/src/main/java/com/klimbi123/tickettesttask/MainActivity.java
+++ app/src/main/java/com/klimbi123/tickettesttask/MainActivity.java
@@ -7,11 +7,13 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView responseText;
     private EditText productNameField;
     private EditText priceField;
     private Button addTicketButton;
@@ -36,16 +38,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        responseText = findViewById(R.id.responseText);
         productNameField = findViewById(R.id.productNameField);
         priceField = findViewById(R.id.priceField);
         addTicketButton = findViewById(R.id.addTicketButton);
 
         addTicketButton.setOnClickListener(view -> {
+            TicketClass.Ticket ticket = TicketClass.Ticket.newBuilder()
+                    .setId(5)
+                    .setProductName(productNameField.getText().toString())
+                    .setPrice(Integer.parseInt(priceField.getText().toString()))
+                    .build();
+
             String message = String.format("%s - %s", productNameField.getText().toString(), priceField.getText().toString());
             Snackbar.make(view, message, Snackbar.LENGTH_LONG)
                     .setAction("AddTicket", null).show();
+
             productNameField.setText("");
             priceField.setText("");
+
+            responseText.setText(ticket.toString());
         });
 
         productNameField.addTextChangedListener(inputFieldsFilled);
