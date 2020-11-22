3b2612bb788698af4e9880d4e7b8da981b558b8d
Ingmar Trump <datklimbi123@gmail.com>
2020-11-22 12:24:51 +0200

    feat: Ticket sending to URL
    fix: Ticket price in cents

--- app/src/main/java/com/klimbi123/tickettesttask/MainActivity.java
+++ app/src/main/java/com/klimbi123/tickettesttask/MainActivity.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText productNameField;
     private EditText priceField;
     private Button addTicketButton;
+    private TicketClass.Ticket ticket;
 
     private TextWatcher inputFieldsFilled = new TextWatcher() {
         @Override
@@ -44,23 +55,48 @@ public class MainActivity extends AppCompatActivity {
         addTicketButton = findViewById(R.id.addTicketButton);
 
         addTicketButton.setOnClickListener(view -> {
-            TicketClass.Ticket ticket = TicketClass.Ticket.newBuilder()
+            ticket = TicketClass.Ticket.newBuilder()
                     .setId(5)
                     .setProductName(productNameField.getText().toString())
                     .setPrice(Integer.parseInt(priceField.getText().toString()))
                     .build();
 
-            String message = String.format("%s - %s", productNameField.getText().toString(), priceField.getText().toString());
-            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-                    .setAction("AddTicket", null).show();
-
             productNameField.setText("");
             priceField.setText("");
 
-            responseText.setText(ticket.toString());
+            String shortMessage = String.format("Sending: %s - %s", ticket.getProductName(), ticket.getPrice());
+            responseText.setText("Sending");
+            Snackbar.make(view, shortMessage, Snackbar.LENGTH_LONG)
+                    .setAction("AddTicket", null).show();
+            send(ticket);
         });
 
         productNameField.addTextChangedListener(inputFieldsFilled);
         priceField.addTextChangedListener(inputFieldsFilled);
+    }
+
+    private void send(TicketClass.Ticket ticket) {
+        System.out.println("Send start");
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://192.168.8.102:5000/post");
+                    HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                    http.setRequestMethod("POST");
+                    http.setDoOutput(true);
+                    OutputStream out = http.getOutputStream();
+                    ticket.writeTo(out);
+
+                    boolean sentSuccessfully = http.getResponseCode() == 200;
+                    String longMessage = String.format("%s\nProduct name: %s\nPrice (cents): %s", sentSuccessfully ? "Sent" : "Failed", ticket.getProductName(), ticket.getPrice());
+                    responseText.setText(longMessage);
+                    System.out.println(sentSuccessfully ? "Send complete" : "Send failed");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(runnable).start();
     }
 }
--- app/src/test/java/com/klimbi123/tickettesttask/ExampleUnitTest.java
+++ app/src/test/java/com/klimbi123/tickettesttask/ExampleUnitTest.java
@@ -1,6 +1,19 @@
 package com.klimbi123.tickettesttask;
 
+import android.util.Log;
+
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.RunnableFuture;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +23,44 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private static String readStream(InputStream in) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            String nextLine = "";
+            while ((nextLine = reader.readLine()) != null) {
+                sb.append(nextLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
     @Test
-    public void addition_isCorrect() {
+    public void addition_isCorrect() throws IOException {
         assertEquals(4, 2 + 2);
+
+        TicketClass.Ticket ticket = TicketClass.Ticket.newBuilder()
+                .setId(5)
+                .setProductName("TicketName")
+                .setPrice(Integer.parseInt("512"))
+                .build();
+
+        URL url = new URL("http://192.168.8.102:5000/post");
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        OutputStream out = http.getOutputStream();
+        ticket.writeTo(out);
+
+        System.out.println(http.getResponseCode());
+        for (int i = 0; i < 8; i++) {
+            System.out.println(http.getHeaderFieldKey(i) + " - " + http.getHeaderField(i));
+        }
+        String readStream = readStream(http.getInputStream());
+        System.out.println(readStream);
+
+        http.disconnect();
     }
 }
